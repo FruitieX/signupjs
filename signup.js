ef5004fa72eb87e4b0f25c71295893c17be3f669
Rasmus Eskola <fruitiex@gmail.com>
2014-10-25 02:33:54 +0300

    limit featureset to a REST API

--- signup.js
+++ signup.js
@@ -2,57 +2,64 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 var fs = require('fs');
-
 var dirname = __dirname + '/tournies';
-
 if(!fs.existsSync(dirname))
     fs.mkdirSync(dirname);
 
 app.get('/', function(req, res) {
-    res.send('Please provide tourney ID!');
+    res.render('usage');
 });
+
+var getEntries = function(tourney) {
+    var path = dirname + '/' + tourney + '.json';
+    var entries = [];
+
+    if(fs.existsSync(path))
+        entries = JSON.parse(fs.readFileSync(path).toString());
+
+    return entries;
+};
+
+appendEntry = function(tourney, entry) {
+    // validate types
+    if(entry.team) {
+        // 'nicks' must be an array no shorter than two cells
+        if (!(entry.nicks instanceof Array) || entry.nicks.length < 2)
+            return false;
+    } else {
+        // 'nick' must be a string
+        if(typeof entry.nick !== 'string')
+            return false;
+    }
+
+    var entries = getEntries(tourney);
+
+    // check if entry exists, based on nick & team property, if so: quit
+    for(var i = 0; i < entries.length; i++)
+        if((entries[i].nick && entries[i].nick === entry.nick) ||
+           (entries[i].team && entries[i].team === entry.team))
+            return true;
+
+    // write entry to file
+    entries.push(entry);
+    fs.writeFileSync(dirname + '/' + tourney + '.json', JSON.stringify(entries));
+    return true;
+};
 
 app.get('/:tourney', function(req, res) {
-    var path = dirname + '/' + req.params.tourney + '.txt';
-    var entries = '';
-
-    if(fs.existsSync(path))
-        entries = fs.readFileSync(path).toString();
-
-    entries = entries.split('\n');
-    entries.pop(); // trailing newline causes empty entry
-
-    res.render('index', { tourney: req.params.tourney, entries: entries });
+    res.json(getEntries(req.params.tourney));
 });
 
-app.post('/:tourney', function(req, res) {
-    if(!req.params.tourney || req.params.tourney === '' ||
-       !req.body.nick    || req.body.nick === '') {
-
-        res.send('Please provide nick');
-        return;
+app.post('/:tourney', bodyParser.json(), function(req, res) {
+    if(appendEntry(req.params.tourney, req.body)) {
+        res.send('Signup completed');
+    } else {
+        res.status(400).send('Signup failed. Check "/" for usage instructions');
     }
-
-    var path = dirname + '/' + req.params.tourney + '.txt';
-    var entries = '';
-
-    if(fs.existsSync(path))
-        entries = fs.readFileSync(path).toString();
-
-    entries = entries.split('\n');
-    entries.pop(); // trailing newline in file causes empty entry
-
-    if(entries.indexOf(req.body.nick) === -1) {
-        fs.writeFileSync(path, req.body.nick + '\n', { flag: 'a' });
-        entries.push(req.body.nick);
-    }
-
-    res.render('index', { tourney: req.params.tourney, entries: entries });
 });
 
 app.listen(process.env.PORT || 8080);
